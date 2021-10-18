5a4cba90ae0d044895e096c1f37f14d96876402a
hosseinam <hosseinam@pnr.ir>
2021-10-18 16:54:37 +0330

    create main_app application

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main_app.views import *
+
+app_name = 'main_application'
+
+urlpatterns = [
+    path('', index_view, name='index_url'),
+    path('changer', changer_view, name='changer_url'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_GET, require_POST
+from base_settings.models import APPLICATIONS
+from facebook_scraper import set_cookies, get_posts, get_profile, get_friends, get_page_info, get_group_info, get_photos
+from mcrawler.json_response import decorator as returner
 
-# Create your views here.
+cookie = {
+    'c_user': '100002423197632',
+    'xs': '44%3Ag6UTHg8W4ZdUdA%3A2%3A1634555056%3A-1%3A9726',
+}
+
+
+@require_GET
+@login_required
+def index_view(request: HttpRequest):
+    apps_name = APPLICATIONS.keys()
+    apis_name = APPLICATIONS['FaceBook'].keys()
+    return render(request, 'main_app/index.html', context={
+        'apps_name': apps_name,
+        'apis_name': apis_name,
+    })
+
+
+@returner
+@require_POST
+@login_required
+def changer_view(request: HttpRequest):
+    socks1 = request.POST['socks1']
+    socks2 = request.POST['socks2']
+
+    username = request.POST['username']
+    password = request.POST['password']
+
+    account_id = request.POST['account_id']
+
+    app_name = request.POST['app_name']
+    api_name = request.POST['api_name']
+    return switch(app_name, api_name, account_id)
+
+
+def switch(app_name, api_name, account_id):
+    if app_name == 'FaceBook':
+        set_cookies(cookie)
+        if api_name == 'Profile':
+            return get_profile(account=account_id)
+        if api_name == 'Friends':
+            return get_friends(account=account_id)
+        if api_name == 'Page Information':
+            return get_page_info(account=account_id)
+        if api_name == 'Group Information':
+            return get_group_info(group=account_id)
+        if api_name == 'Posts':
+            return get_posts(account=account_id)
+        if api_name == 'Photos':
+            return get_photos(account=account_id)
+        else:
+            return None
+    else:
+        return None
