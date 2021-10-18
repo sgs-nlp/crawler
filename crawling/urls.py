a6c4c8cb0b4671a33126bf0e0d8bb9d0f95ceac3
hosseinam <hosseinam@pnr.ir>
2021-10-18 16:45:18 +0330

    set urls base_settings

--- crawling/urls.py
+++ crawling/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('crawling/linkedin/', include('linked_in.urls')),
-]
+                  path('', include('main_app.urls')),
+                  path('settings/', include('base_settings.urls')),
+                  path('admin/', admin.site.urls),
+                  path('user/', include('users.urls')),
+                  path('facebook/', include('facebook.urls')),
+                  path('linkedin/', include('linked_in.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
