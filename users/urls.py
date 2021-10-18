fff6c5d5f74feb620e5a8915eda1d23e5f7c5923
hosseinam <hosseinam@pnr.ir>
2021-10-18 16:55:47 +0330

    create user application

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login', login_view, name='login_url'),
+    path('login/done', login_done, name='login_done_url'),
+    path('logout', logout_done, name='logout_url'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate as dj_auth, login as dj_login, logout as dj_logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST, require_GET
 
-# Create your views here.
+
+@require_GET
+def login_view(request: HttpRequest):
+    return render(request, 'users/login.html')
+
+
+@require_POST
+def login_done(request: HttpRequest):
+    if not request.user.is_anonymous:
+        return redirect('main_application:index_url')
+    if 'username' in request.POST and len(request.POST['username']) != 0:
+        username = request.POST['username']
+    else:
+        return redirect('users:login_url')
+    if 'password' in request.POST and len(request.POST['password']) != 0:
+        password = request.POST['password']
+    else:
+        return redirect('users:login_url')
+
+    user = dj_auth(username=username, password=password)
+    if user is not None:
+        dj_login(request, user)
+        return redirect('main_application:index_url')
+    return redirect('users:login_url')
+
+
+@require_POST
+@login_required
+def logout_done(request: HttpRequest):
+    dj_logout(request)
+    return redirect('users:login_url')
