3b6b8748a67a61c4fc3eb964f090bc351644889d
hosseinam <hosseinam@pnr.ir>
2021-10-19 13:22:11 +0330

    delete linked in url

--- facebook/api/urls.py
+++ facebook/api/urls.py
@@ -1,10 +1,12 @@
+"""
+facebook.api.urls.py
+"""
 from django.urls import path
+
 from facebook.api.views import *
 
 app_name = 'facebook_crawler_api'
 urlpatterns = [
-
-    path('set-cookies', set_cookies_api_view, name='set_cookies_api_view'),
     path('get-profile', get_profile_api_view, name='get_profile_api_view'),
     path('get-friends', get_friends_api_view, name='get_friends_api_view'),
     path('get-page-info', get_page_info_api_view, name='get_page_info_api_view'),
